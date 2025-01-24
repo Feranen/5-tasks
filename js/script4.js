dca80616b0984692ecd0d56d85f359a65bc4904c
Feranen <127853064+Feranen@users.noreply.github.com>
2025-01-24 16:42:51 +0100

    Update

--- js/script4.js
+++ js/script4.js
@@ -3,6 +3,11 @@ function use() {
     var b = parseFloat(document.getElementById('side2').value);
     var c = parseFloat(document.getElementById('side3').value);
 
+    if ((isNaN(a)) || isNaN(b) || isNaN(c)){
+            document.getElementById('result').innerText = `Enter three numbers`
+            return
+    }
+
     const min = Math.min(a, b, c);
     const max = Math.max(a, b, c);
 
