67c0cd6d1d63117a4642435417a1e3a0aba55c26
Feranen <127853064+Feranen@users.noreply.github.com>
2025-01-24 14:56:00 +0100

    Fully completed

---
+++ js/script5.js
@@ -0,0 +1,12 @@
+function check(){
+    let word = document.getElementById("side1").value
+    const result = document.getElementById("result")
+    if (word.length >= 6 && word.endsWith('kot')) {
+        result.innerText = `Word has 6 characters and end with "kot"`
+    } else{
+        result.innerText = `Word hasn't 6 characters neither end with "kot"`
+    }
+
+
+}
+
