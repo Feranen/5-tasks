69cc9e2f8a942295dd8d6fe7daa570714f9b68f0
Feranen <127853064+Feranen@users.noreply.github.com>
2025-01-24 09:29:58 +0100

    Task 2

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+function checkPrediction() {
+    let probability = Math.floor(Math.random() * 100) + 1;
+    const resultelement = document.getElementById('result')
+    resultelement.textContent = `Prawdopodobienstwo, że tak będzie wynosi: ${probability}%`; 
+    if (probability <= 33) {
+        resultelement.className = "result low";
+    } else if (probability <= 66) {
+        resultelement.className = "result medium"
+    } else {
+        resultelement.className = "result high"
+    }
+
+}
