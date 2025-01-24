8d070ec6368e87e73f094583743a1bf6b1a89e39
Feranen <127853064+Feranen@users.noreply.github.com>
2025-01-24 14:19:10 +0100

    Task 3

---
+++ js/script3.js
@@ -0,0 +1,13 @@
+function replace() {
+    let word1 = document.getElementById('first').value
+    let word2 = document.getElementById('second').value
+
+    if (word1.length > 0 && word2.length > 0) {
+        const newword1 = word2.charAt(0) + word1.slice(1);
+        const newword2 = word1.charAt(0) + word2.slice(1);
+        document.getElementById('result').innerText = `Wynik: ${newword1} ${newword2}`
+    }   else {
+        document.getElementById('result').innerText = `Podaj oba słowa`
+    }
+
+}
